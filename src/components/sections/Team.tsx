6891aa32abac0233c4826014ec06514e0a369672
Daniel <danieldimp@hotmail.com>
2026-01-14 13:59:33 -0600

    Reemplazar Luis por Aldo en el equipo

--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -10,8 +10,8 @@ const team = [
     image: '/images/team/elisabeth.png',
   },
   {
-    name: 'Luis',
-    image: '/images/team/luis.png',
+    name: 'Aldo',
+    image: '/images/team/aldo.png',
   },
   {
     name: 'Emilio',
@@ -24,10 +24,6 @@ const team = [
   {
     name: 'Osvaldo',
     image: '/images/team/osvaldo.png',
-  },
-  {
-    name: 'Aldo',
-    image: '/images/team/aldo.png',
   },
 ]
 
@@ -60,7 +56,7 @@ export default function Team() {
             <motion.div
               key={member.name}
               className={`flex flex-col items-center ${
-                index === 5 ? 'col-span-2 md:col-span-1 w-[calc(50%-12px)] md:w-full mx-auto' : ''
+                index === 4 ? 'col-span-2 md:col-span-1 w-[calc(50%-12px)] md:w-full mx-auto' : ''
               }`}
               initial={{ opacity: 0, y: 30 }}
               animate={isInView ? { opacity: 1, y: 0 } : {}}
